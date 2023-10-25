e4f85f5058e84f8d51fefa9e0fe6ced6c83ed1a1
1wes <okemwawes@gmail.com>
2023-10-25 13:33:10 +0300

    display messages in bubbles

--- src/chat.jsx
+++ src/chat.jsx
@@ -1,4 +1,4 @@
-import { Fragment, useState, useEffect } from "react";
+import { Fragment, useState, useEffect, useRef } from "react";
 
 import './chat.css'
 import deadDrop from './assets/deaddrop.svg'
@@ -7,14 +7,47 @@ import { useParams } from "react-router-dom";
 
 import { LuSendHorizonal } from 'react-icons/lu';
 
+
 const ChatArea = () => {
 
-    const [messages, setMessage] = useState([]);
+    const [messages, setMessages] = useState([]);
     const [messageBody, setMessageBody] = useState("");
 
     const username = useParams().username;
 
+    const ws = useRef();
+    const latestMessage = useRef(null);
+
     useEffect(() => {
+        
+        if (latestMessage.current) {
+            latestMessage.current.scrollIntoView({
+                behavior:"smooth"
+            })
+        }
+    },[messages.length])
+
+    useEffect(() => {
+
+        ws.current = new WebSocket("ws://localhost:8080");
+
+        ws.current.onopen = () => {
+            console.log("connection opened");
+        }
+
+        ws.current.onmessage = (ev) => {
+            
+            const message = JSON.parse(ev.data);
+
+            setMessages((_messages) => [..._messages, message]);
+        }
+
+        return () => {
+            
+            console.log("Cleaning up...")
+
+            ws.current.close();
+        }
         
     }, []);
 
@@ -27,7 +60,13 @@ const ChatArea = () => {
         
         e.preventDefault();
 
-        alert(messageBody);
+        // dont send empty messages
+        if (messageBody !== "") {
+            ws.current.send(JSON.stringify({
+                sender: username,
+                body: messageBody
+            }));
+        }
 
         setMessageBody("");
     }
@@ -43,7 +82,25 @@ const ChatArea = () => {
                     <h2>{ username }</h2>
                 </div>
                 <div className="chat-area">
-
+                    {
+                        messages.map((message) => (
+                            
+                            <article key={message.sentAt} className={message.sender===username?"me":"receiver"} >
+                                <header className="bubble-header">
+                                    <h4 className="sender">
+                                        {message.sender===username?"You":message.sender}
+                                    </h4>
+                                    <span className="timestamp"> 
+                                        {new Date(message.sentAt).toLocaleTimeString(undefined, {timeStyle:"short"})}
+                                    </span>
+                                </header>
+                                <p className="message-body">
+                                    {message.body}
+                                </p>
+                            </article>
+                        ))
+                    }
+                    <div ref={latestMessage} ></div>
                 </div>
                 <div className="message-input">
                     <form className="message" onSubmit={sendMessage} > 
