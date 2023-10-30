300454bed8560d229015f2133c92638094184118
1wes <okemwawes@gmail.com>
2023-10-30 16:31:34 +0300

    export db details

--- server/env-config.js
+++ server/env-config.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 
-const { PORT } = process.env;
+const { PORT, DB_USERNAME, DB_HOST, DB_PASSWORD, DB_NAME } = process.env;
 
 module.exports = {
-    port:PORT
+    port: PORT,
+    db_username: DB_USERNAME,
+    db_host: DB_HOST,
+    db_password: DB_PASSWORD,
+    db_name:DB_NAME
 }
