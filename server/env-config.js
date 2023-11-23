75ea0f6a7b3e34686b98d63cebc05bb1aaa584f6
1wes <okemwawes@gmail.com>
2023-11-23 12:16:16 +0300

    export session secret

--- server/env-config.js
+++ server/env-config.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 
-const { PORT, DB_USERNAME, DB_HOST, DB_PASSWORD, DB_NAME } = process.env;
+const { PORT, DB_USERNAME, DB_HOST, DB_PASSWORD, DB_NAME, SESSION_SECRET } = process.env;
 
 module.exports = {
     port: PORT,
     db_username: DB_USERNAME,
     db_host: DB_HOST,
     db_password: DB_PASSWORD,
-    db_name:DB_NAME
+    db_name: DB_NAME,
+    session_secret:SESSION_SECRET
 }
