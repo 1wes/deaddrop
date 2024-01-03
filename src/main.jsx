f0fc7985898108f9a22e4542c809a2ed97c642a6
1wes <okemwawes@gmail.com>
2024-01-03 15:53:49 +0300

    import SocketContextProvider as the default export package

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ChatArea from './chat.jsx'
-import { SocketContextProvider } from './context/socketContext.jsx'
+import SocketContextProvider from './context/socketContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <SocketContextProvider>
